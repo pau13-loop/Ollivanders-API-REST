9d43505f322e5b36f88fb07d86f366194c523faf
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-18 21:05:25 +0200

    test: :white_check_mark: Add test_get_by_quality test cases file

--- test/test_sql/test_repository/test_repository_db.py
+++ test/test_sql/test_repository/test_repository_db.py
@@ -155,24 +155,3 @@ def test_get_by_sellin_db(session):
     assert get_conjured_by_sellin.sell_in == 5
     assert get_conjured_by_sellin.quality == 8
 
-
-@pytest.mark.db_test
-def test_get_by_quality_db(session):
-    """Test if through session we can get an item by its quality
-
-    Args:
-        session (SQLAlchemy Object Session): It's the session object from SQLALchemy Instance
-    """
-
-    # Añadimos el item
-    add_item = Items("Conjured Mana Cake", 5, 8)
-
-    session.add(add_item)
-    session.commit()
-
-    # Get el item por su calidad, en este caso como solo hay un item de esa calidad, retornará el del conjured mana cake
-    get_conjured_by_quality = session.query(Items).filter(Items.quality == 8).first()
-
-    assert get_conjured_by_quality.name == "Conjured Mana Cake"
-    assert get_conjured_by_quality.sell_in == 5
-    assert get_conjured_by_quality.quality == 8
